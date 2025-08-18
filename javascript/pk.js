47c4be119ed3e44cb4c13784c9d297e9de7cb4ff
autmku62622bcs31 <praveencm329@gmail.com>
2025-08-18 20:18:10 +0530

    first commit

---
+++ javascript/pk.js
@@ -0,0 +1,13 @@
+/*rentbysport*/
+function gotocricketpage(){
+    window.location.href = "http://127.0.0.1:5500/html/cricrent.html"
+}
+function gotofootpage(){
+    window.location.href = "http://127.0.0.1:5500/html/football.html"
+}
+function gototennispage(){
+    window.location.href = "http://127.0.0.1:5500/html/tennisrent.html"
+}
+function gotovolleypage(){
+    window.location.href = "http://127.0.0.1:5500/html/volrent.html"
+}
---
+++ javascript/rentpage.js
@@ -0,0 +1,5 @@
+/*mrfbatbuying*/
+function gotomrfbatpage(){
+    window.location.href = "http://127.0.0.1:5500/html/buying.html"
+}
+
