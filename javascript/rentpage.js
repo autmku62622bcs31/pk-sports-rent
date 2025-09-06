cb2afd2c851bf5f5a18dba30892a461f4b94afe6
autmku62622bcs31 <praveencm329@gmail.com>
2025-09-06 07:57:29 +0530

    first commit

--- javascript/rentpage.js
+++ javascript/rentpage.js
@@ -1,5 +1,23 @@
 /*mrfbatbuying*/
 function gotomrfbatpage(){
-    window.location.href = "http://127.0.0.1:5500/html/buying.html"
+    window.location.href = "http://127.0.0.1:5500/html/buyingmrf.html"
 }
 
+function gotovectorkitpage(){
+    window.location.href = "http://127.0.0.1:5500/html/buyingvec.html"
+}
+function gototenbat(){
+    window.location.href = "http://127.0.0.1:5500/html/buyingtenbat.html"
+}
+
+function gotopumafoot(){
+    window.location.href = "http://127.0.0.1:5500/html/buyingpumafoot.html"
+}
+
+function gotovolley(){
+    window.location.href = "http://127.0.0.1:5500/html/buyingvolley.html"
+}
+
+function gotossbat(){
+    window.location.href = "http://127.0.0.1:5500/html/buyingssbat.html"
+}
