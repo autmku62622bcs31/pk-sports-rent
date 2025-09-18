0856c8b9c4bd4dbcf07dadeb567a13938d8eedde
autmku62622bcs31 <praveencm329@gmail.com>
2025-09-18 22:59:47 +0530

    first commit

---
+++ javascript/crickrent.js
@@ -0,0 +1,15 @@
+function gotosgbatpage(){
+    window.location.href = "http://127.0.0.1:5500/html/buyingsg.html"
+}
+function gotoreebokbatpage(){
+    window.location.href = "http://127.0.0.1:5500/html/buyingreebok.html"
+}
+function gotoceatbatpage(){
+    window.location.href = "http://127.0.0.1:5500/html/buyingceat.html"
+}
+function gotokookabatpage(){
+    window.location.href = "http://127.0.0.1:5500/html/buyingkooka.html"
+}
+function gotossbat(){
+    window.location.href = "http://127.0.0.1:5500/html/buyingssbat.html"
+}
