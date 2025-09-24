fc30323d3ddd7c0a61c3c43e3342095849b4fe5c
autmku62622bcs31 <praveencm329@gmail.com>
2025-09-24 12:57:00 +0530

    first commit

--- javascript/volley.js
+++ javascript/volley.js
@@ -1,3 +1,18 @@
 function gotovolley(){
     window.location.href = "http://127.0.0.1:5500/html/buyingvolley.html"
 }
+function gotocoscovoll(){
+    window.location.href = "http://127.0.0.1:5500/html/buyingcosco.html"
+}
+function gotoninevoll(){
+    window.location.href = "http://127.0.0.1:5500/html/buying9ball.html"
+}
+function gotospartanvoll(){
+    window.location.href = "http://127.0.0.1:5500/html/buyingspartan.html"
+}
+function gotoclassicvoll(){
+    window.location.href = "http://127.0.0.1:5500/html/buyingclassicball.html"
+}
+function gotomikazavoll(){
+    window.location.href = "http://127.0.0.1:5500/html/buyingclassicball.html"
+}
