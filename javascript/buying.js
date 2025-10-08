fe89bcd2d2298b375676c8f5320623954de5ca39
autmku62622bcs31 <praveencm329@gmail.com>
2025-10-08 21:29:36 +0530

    first commit

--- javascript/buying.js
+++ javascript/buying.js
@@ -1,3 +1,34 @@
-function gotoformpage(){
-    window.location.href = "http://127.0.0.1:5500/html/form.html"
-}
+// function gotoformpage(){
+//     window.location.href = "http://127.0.0.1:5500/html/form.html"
+// }
+
+//     function getDifferenceInDays(d1, d2) {
+//       if (!d1 || !d2) return "";
+//       const diffTime = Math.abs(new Date(d2) - new Date(d1));
+//       const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+//       return diffDays;
+//     }
+
+//     const date1 = document.getElementById("date1");
+//     const date2 = document.getElementById("date2");
+//     const result = document.getElementById("result");
+
+//     function updateDifference() {
+//       const days = getDifferenceInDays(date1.value, date2.value);
+//       result.value = days; 
+//     }
+//     date1.addEventListener("input", updateDifference);
+//     date2.addEventListener("input", updateDifference);
+
+        //     document.getElementById('bookbtn').addEventListener('click', function() {
+        //     const date1 = document.getElementById('date1').value;
+        //     const date2 = document.getElementById('date2').value;
+
+        //     if (date1.trim() !== '' && date2.trim() !== '')  {
+        //         // Both input boxes are filled, navigate to the next page
+        //         window.location.href = "http://127.0.0.1:5500/html/form.html" // Replace with your next page URL
+        //     } else {
+        //         // Display an alert or message if inputs are not filled
+        //         alert('Please fill both input boxes before proceeding.');
+        //     }
+        // });
